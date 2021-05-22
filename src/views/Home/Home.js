2eef9cad9988dc0642321d20d453ab2413135643
yoda-k <k-hole-yoda@protonmail.com>
2021-05-22 10:57:30 +0200

    Add Leaderboard View

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -175,8 +175,6 @@ export default function Home() {
                                                     <Button>Trade</Button>
                                                 </Link>
                                             </TableCell>
-
-
                                         </TableRow>
                                     ))}
                                 </TableBody>
